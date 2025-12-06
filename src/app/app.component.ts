f34d0d8d803270de11f0d81293bb606e88c63f07
amrita-priya08 <amrita.priya2014@gmail.com>
2025-12-06 22:01:45 +0530

    input and output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
-
+import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "./task/task.component";
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'task-management';
+  users = DUMMY_USERS;
+  selectedUserId = ""
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+  onSelectUser(id: string){
+    this.selectedUserId = id
+    console.log("userr",this.selectedUserId)
+  }
+
 }
